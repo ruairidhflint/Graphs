bd23e045982f4755b9d232567cde1ca4ea97576d
Rory Flint <roryflintphoto@gmail.com>
2020-03-20 08:23:29 +0000

    Add queue class

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -8,6 +8,19 @@ from ast import literal_eval
 import os
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
 # Load world
 world = World()
 
