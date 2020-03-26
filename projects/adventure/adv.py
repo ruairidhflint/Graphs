acdd39fee598e77d19d508ec03205cf275838a75
Rory Flint <roryflintphoto@gmail.com>
2020-03-26 16:04:45 +0000

    Conplete re-psuedo code as previous had too many unknowns

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -63,12 +63,12 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 my_file = os.path.join(THIS_FOLDER, map_file)
 
 # Loads the map into a dictionary
@@ -80,63 +80,51 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
-# Reverse directions to save logic later
-# reverse_directions = {'n': 's', 's': 'n', 'e':'w', 'w':'e'}
-
-# Dictionary for traversal path (could be a set?)
-# traversal_path = {}
-
-
-# Use a stack to begin with, queue later if necessary
-# s = Stack()
-# Keep track of the last room id
-# previous_room = None
-# Keep track of the direction we came from
-# previous_direction = None
-
-
-# Whilst the traversal graph is smaller than the room graph itself, we need to keep exploring
-
-    # Get current room the player is in
-
-    # Check if it has been traversed or not
-        # If not make a list of the room's exits using getExits()
-        # Add room to traversal graph with  the exits the value in a dict
-    
-    # Check for previous room
-        # If there is one, we need to update it's value for the direction we travelled to 
-        # current room ID
-        # Give the current room's opposite direction value to match
-    # Previous room should be updated to the current room
-
-    # Check to see if we can actually go anywhere and if we have tried yet
-
-    # Log which directions we can move
-    
-    # If we can go nowhere, go back to the previous room and try somewhere else.
-
-
+# Traversal path as list not DICT
 traversalPath = []
-opposite_directions = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
-rooms = {}
-visited = set()
+
+# DICT of rooms instead of Traversal path
+# As in guided lecture, a visited set
 
 
+# Create a depth first search - takes in current room, the direction we came from and the previous room's ID
+
+# add current room's id tov visited set
+
+# set the temp room dict with all the cardinal values set to ?
+
+# Loop through the get_exits() functionality and check if any exist
+
+# if the current room id is not in the room dict
+
+# set the current room's id to the room dict
+
+# if there is a previous direction
+
+# set the oppsoite direction and current direction to match between rooms
+# Loop through the rooms that are abilable and try to travel to any with a ?
 
 
+# Breadth first search on a current room
+# Create quite
+# Add list with current room is
+# Make temp set
+# whilst queue is larger than zero
+# dequeue to temp var
+# current id is the last value
+# if that is is not in visited
+# make a new list of the room's values
+# If any of the values are ? or the length's or the room graph match the visited set
+# return that path
+# add to the rooms visited the current room id
+# loop through the the rooms held in the values list
+# go through the motions of adding a new list that is a copy and enqueueing
 
-
-
-
-
-
-
-
-
-
-
-
-
+# FUnction to bring it all together
+# whilst the length of the room graph is not equal to the length of the visited,
+# DO a depth first search starting at the current room.
+# The path is the same except a breadth first search
+# Loop through every single room in the path, adding the relevant valyes to the traversal path
 
 
 # TRAVERSAL TEST
